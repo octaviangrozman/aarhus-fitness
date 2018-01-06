156d78ef174793cfa1fdefacc2f733613e1476c2
octaviangrozman <octaviangrozman@yahoo.com>
2018-01-06 20:46:42 +0100

    Sign up functionality added Sign in activity updated

--- app/src/main/java/com/example/octav/aarhusfitness/LoginActivity.java
+++ app/src/main/java/com/example/octav/aarhusfitness/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.octav.aarhusfitness;
 
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -12,9 +14,14 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -24,11 +31,13 @@ import com.google.firebase.auth.FirebaseUser;
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "UserSignIn";
+    public static final String PREFS = "prefs";
+    public static final String FITNESS_PLACE = "fitnessPlace";
 
-    // UI references.
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
-    private View mLoginFormView;
+    // ui
+    private AutoCompleteTextView emailAutoCompleteTextView;
+    private EditText passwordEditText;
+    private ProgressBar progressBar;
 
     // Firebase
     private FirebaseAuth mAuth;
@@ -41,33 +50,50 @@ public class LoginActivity extends AppCompatActivity {
         // Firebase auth
         mAuth = FirebaseAuth.getInstance();
 
-        // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
 
-        mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
-                    return true;
-                }
-                return false;
-            }
-        });
+        // Build a GoogleSignInClient with the options specified by gso.
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+        emailAutoCompleteTextView = findViewById(R.id.email);
+        passwordEditText = findViewById(R.id.password);
+        progressBar = findViewById(R.id.progress_bar);
+
+        TextView signUpTextView = (TextView) findViewById(R.id.sign_up_text_view);
+        signUpTextView.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
             }
         });
 
-        mLoginFormView = findViewById(R.id.login_form);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+                GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+//                Intent intent = new Intent(LoginActivity.this, FitnessPlaceActivity.class);
+//                startActivity(intent);
+    }
+
+    public void signInButtonOnClick(View view) {
+        signIn(String.valueOf(emailAutoCompleteTextView.getText()), String.valueOf(passwordEditText.getText()));
+    }
+
+    public void signInGoogleButtonOnClick(View view) {
 
     }
 
     public void signIn(String email, String password) {
+        progressBar.setVisibility(View.VISIBLE);
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -76,16 +102,29 @@ public class LoginActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-//                            updateUI(user);
+                            startNextActivity();
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
+                            Toast.makeText(LoginActivity.this, "Authentication failed. " + task.getException(),
+                                    Toast.LENGTH_LONG).show();
                         }
-
+                        progressBar.setVisibility(View.GONE);
                     }
                 });
+    }
+
+    public void startNextActivity() {
+        SharedPreferences prefs = getSharedPreferences(PREFS, MODE_PRIVATE);
+        String fitnessPlace = prefs.getString(FITNESS_PLACE, null);
+        if(fitnessPlace != null) {
+            Intent intent = new Intent(LoginActivity.this, FitnessPlaceActivity.class);
+            intent.putExtra(FITNESS_PLACE, fitnessPlace);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+            startActivity(intent);
+        }
     }
 }
 
--- app/src/main/java/com/example/octav/aarhusfitness/SignUpActivity.java
+++ app/src/main/java/com/example/octav/aarhusfitness/SignUpActivity.java
@@ -1,9 +1,19 @@
 package com.example.octav.aarhusfitness;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -12,11 +22,20 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Map;
+
 public class SignUpActivity extends AppCompatActivity {
 
     private static final String TAG = "UserSignUp";
+    public static final String PREFS = "prefs";
+    public static final String FITNESS_PLACE = "fitnessPlace";
 
     private FirebaseAuth mAuth;
+
+    // ui
+    private AutoCompleteTextView emailAutoCompleteTextView;
+    private EditText passwordEditText;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +44,30 @@ public class SignUpActivity extends AppCompatActivity {
 
         // firebase auth
         mAuth = FirebaseAuth.getInstance();
+
+        // init ui elements
+        // Set up the login form.
+        emailAutoCompleteTextView = findViewById(R.id.email);
+        passwordEditText = findViewById(R.id.password);
+        progressBar = findViewById(R.id.progress_bar);
+
+        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    public void signUpButtonOnClick(View view) {
+        createUser(String.valueOf(emailAutoCompleteTextView.getText()), String.valueOf(passwordEditText.getText()));
     }
 
     public void createUser(String email, String password) {
+        progressBar.setVisibility(View.VISIBLE);
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -36,14 +76,31 @@ public class SignUpActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-//                            updateUI(user);
+                            Log.d("USER", user.toString());
+                            Toast.makeText(SignUpActivity.this, "Your account has been succesfully created",
+                                    Toast.LENGTH_SHORT).show();
+                            startNextActivity();
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SignUpActivity.this, "Authentication failed. " + task.getException(),
+                                    Toast.LENGTH_LONG).show();
                         }
+                        progressBar.setVisibility(View.GONE);
                     }
                 });
+    }
+
+    public void startNextActivity() {
+        SharedPreferences prefs = getSharedPreferences(PREFS, MODE_PRIVATE);
+        String fitnessPlace = prefs.getString(FITNESS_PLACE, null);
+        if(fitnessPlace != null) {
+            Intent intent = new Intent(SignUpActivity.this, FitnessPlaceActivity.class);
+            intent.putExtra(FITNESS_PLACE, fitnessPlace);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(SignUpActivity.this, HomeActivity.class);
+            startActivity(intent);
+        }
     }
 }
