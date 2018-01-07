b1ec1e2ad0df075732f8140881efbf03b0b434f2
octaviangrozman <octaviangrozman@yahoo.com>
2018-01-07 12:48:49 +0100

    DateTime pickers added to FitnessPlaceActivity

--- app/src/main/java/com/example/octav/aarhusfitness/FitnessPlaceActivity.java
+++ app/src/main/java/com/example/octav/aarhusfitness/FitnessPlaceActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -15,8 +16,12 @@ import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 
-public class FitnessPlaceActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class FitnessPlaceActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     private FirebaseAuth mAuth;
     FirebaseDatabase database;
@@ -53,7 +58,7 @@ public class FitnessPlaceActivity extends AppCompatActivity {
         TextView textViewFitnessPlace = (TextView) findViewById(R.id.textViewFitnessPlace);
         textViewFitnessPlace.setText(fitnessPlace);
 
-        Button buttonJoin = (Button) findViewById(R.id.buttonJoin);
+        Button buttonJoin = findViewById(R.id.buttonJoin);
         buttonJoin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -95,7 +100,42 @@ public class FitnessPlaceActivity extends AppCompatActivity {
     }
 
     public void joinTraining() {
+        showDatePicker();
 //        database.getReference("trainings").child(new Date().toString()).push();
+    }
+
+    private void showDatePicker() {
+        Calendar now = Calendar.getInstance();
+        DatePickerDialog datePicker = DatePickerDialog.newInstance(
+                this,
+                now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH)
+        );
+        datePicker.setTitle("Pick a date for your next training");
+        datePicker.setVersion(DatePickerDialog.Version.VERSION_1);
+        datePicker.show(getFragmentManager(), "Datepickerdialog");
+    }
+
+    private void showTimePicker() {
+        Calendar now = Calendar.getInstance();
+        TimePickerDialog timePicker = TimePickerDialog.newInstance(
+                this,
+                now.get(Calendar.HOUR_OF_DAY),
+                now.get(Calendar.MINUTE),
+                false
+        );
+        timePicker.setTitle("Pick a time for your next training");
+        timePicker.setVersion(TimePickerDialog.Version.VERSION_1);
+        timePicker.show(getFragmentManager(), "Timepickerdialog");
+    }
+
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        String date = "You picked the following date: "+dayOfMonth+"/"+(monthOfYear+1)+"/"+year;
+        Log.i("DATEPICKER", date);
+        showTimePicker();
     }
 
     public void startLoginActivity() {
@@ -103,4 +143,9 @@ public class FitnessPlaceActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @Override
+    public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
+        String time = "You picked the following time: "+hourOfDay+"h"+minute+"m"+second;
+        Log.i("TIMEPICKER", time);
+    }
 }
